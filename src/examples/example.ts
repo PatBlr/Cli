a0dea2bc0fd08a11bcaefb73206f4d0f4414a13e
PatBlr <patrick-bleeker@t-online.de>
2022-09-17 12:02:33 +0200

    changed example

--- src/examples/example.ts
+++ src/examples/example.ts
@@ -4,11 +4,8 @@ async function test() {
   const cli = new Cli();
   // const answer_ask: string = await cli.ask("This is a question");
   // console.log("answer:", answer_ask);
-  cli.write("What kinds of fruits do you like? ");
-  cli.writeln(
-    "([arrow-keys]: navigate, [space]: select, [return]: submit)",
-    "grey"
-  );
+  cli.write("What kind of fruits do you like? ");
+  cli.writeln("(Everyone likes apples)", "grey");
   // let opts = ["Opt1", "Opt2", "Opt3"];
   // let answer = await cli.askOpts(opts);
   // console.log("You chose:", opts[answer]);
